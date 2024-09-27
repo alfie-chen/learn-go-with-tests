67bc5f9ec89322838d7e28feb48f7a4e912baa37
YenHsingChen <YenHsiangChen@fih-foxconn.com>
2024-09-27 16:12:31 +0800

    modify repeat.go

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,7 +2,7 @@ package iteration
 
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 5; i++ {
 		repeated = repeated + character
 	}
 	return repeated

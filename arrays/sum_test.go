0e4ff7bf9f2108fe874c8b3e553e6e5391b3a43f
YenHsingChen <YenHsiangChen@fih-foxconn.com>
2024-10-04 14:12:07 +0800

    add TestSummAll

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -16,3 +16,13 @@ func TestSum(t *testing.T) {
 	})
 
 }
+
+func TestSummAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
